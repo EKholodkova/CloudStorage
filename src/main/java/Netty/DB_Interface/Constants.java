e3c684da5612757890054329c63f1613a8ad4bb9
EKholodkova <alfaborn@mail.ru>
2021-03-21 17:26:43 +0300

    Добавлен класс для связи с бд

---
+++ src/main/java/Netty/DB_Interface/Constants.java
@@ -0,0 +1,8 @@
+package Netty.DB_Interface;
+
+public class Constants {
+    public static final String USERS_TABLE = "users";
+    public static final String USER_ID = "id";
+    public static final String USER_NAME = "user_name";
+    public static final String USER_PASSWORD = "password";
+}
